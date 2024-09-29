aff3201e83acc446444c5b224584d9e21c7925ab
Andrew <90115111+macleanweb@users.noreply.github.com>
2024-09-29 20:21:17 +0200

    feat: wrap app in context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Layout from '~/components/Layout/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AppContextProvider } from '~/context/AppContext'
+import Layout from '~/components/Layout/Layout'
 import '~/styles/globals.css'
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
@@ -8,11 +9,13 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={new QueryClient()}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={new QueryClient()}>
+			<AppContextProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</AppContextProvider>
+		</QueryClientProvider>
+	)
 }
